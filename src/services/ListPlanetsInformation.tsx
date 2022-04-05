d88d764a3016e87dfe54b76a26bcb31ab9bc940a
Duda Santos <83726420+dudasancar@users.noreply.github.com>
2022-04-05 15:40:22 -0300

    limpando codigo

--- src/services/ListPlanetsInformation.tsx
+++ src/services/ListPlanetsInformation.tsx
@@ -1,10 +1,3 @@
-import Mercury from "../assets/Mercury.png";
-import Venus from "../assets/Venus.png";
-import Earth from "../assets/Earth.png";
-import Mars from "../assets/Mars.png";
-import Jupiter from "../assets/Jupiter.png";
-import Saturn from "../assets/Saturn.png";
-import Uranus from "../assets/Uranus.png";
 import api from "./api";
 
 export const getPlanetsInformation = async () => {
